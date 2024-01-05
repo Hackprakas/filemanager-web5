e25b6fce1c6666fbc6b5e1720a571d424f5581c9
PrakashDev <prakash.t.r.2021.cse@ritchennai.edu.in>
2024-01-05 14:08:18 +0000

    tables needs to be organized

---
+++ filemanager/app/page.tsx
@@ -0,0 +1,18 @@
+import {Button} from '@nextui-org/button';
+import React from 'react'
+import Tabless from "./components/table"
+
+
+function page() {
+  return (<>
+ 
+    
+   <div className="bg-white">
+   <Tabless />
+   </div>
+ 
+  </>
+  )
+}
+
+export default page
